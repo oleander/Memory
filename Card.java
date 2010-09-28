f1d0ef03f304decf3f0b1a3ce66128f09ee667b6
Jesper Josefsson <jesper.josefsson@gmail.com>
2010-09-28 16:31:48 +0200

    lade till doc i gitignore, skapade enum för state

--- Card.java
+++ Card.java
@@ -6,21 +6,39 @@ import javax.swing.JButton;
  * @param value innehållet i kortet
  */
  
-
-class Card extends JButton {
+public class Card extends JButton {
   private int id;
   private String value;
-  private boolean flipped;
+  private State state;
+  
+  /**
+   * Skapar ett kort med ansiktet neråt
+   * @param id ett unikt id
+   */
   
   public Card (int id) {
     this.id = id;
+    state = State.DOWN;
   }
+  
+  
   
   public int getId() {
     return id;
   }
   
   public void flip() {
-    
+    if (state == State.UP) {
+    	state = State.DOWN;
+    	this.setText("");
+    }
+    if (state == State.DOWN) {
+    	state = State.UP;
+    	this.setText(value);
+    }
+  }
+  
+  public enum State {
+	  UP, DOWN, INVISIBLE
   }
 }
