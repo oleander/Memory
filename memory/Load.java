41f018e8156b76784e5461826c2fef58f943d5cc
Linus Oleander <linus@oleander.nu>
2010-09-30 17:09:51 +0200

    Lägger till lite kommentarer

--- memory/Load.java
+++ memory/Load.java
@@ -3,16 +3,31 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
+/**
+* Ett valt i menyn
+* Läser inställningarna från en fil
+*/
 class Load extends JMenuItem implements ActionListener {
   
   private Memory memory = null;
   
+  /**
+  * Laddar in inställningarna från en extern fil
+  * Se {memory.load()} för mer information
+  * @param name, Texten som ska placeras på knappen i menyn
+  * @param memory, Ett memory-objekt
+  */
   public Load(String name, Memory memory){
     this.setText(name);
     this.memory = memory;
     this.addActionListener(this);
   }
   
+  /**
+  * Kickas igång när någon klickat på en knapp
+  * Kallar i sin tur på memorys load-metod
+  * @param e, Knappen som klickas på
+  */
   public void actionPerformed(ActionEvent e){
     this.memory.load();
   }
--- memory/Save.java
+++ memory/Save.java
@@ -3,16 +3,31 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
+/**
+* Ett valt i menyn
+* Sparar inställningarna till en fil
+*/
 class Save extends JMenuItem implements ActionListener {
   
   private Memory memory = null;
   
+  /**
+  * Sparar inställningarna från en extern fil
+  * Se {memory.save()} för mer information
+  * @param name, Texten som ska placeras på knappen i menyn
+  * @param memory, Ett memory-objekt
+  */
   public Save(String name, Memory memory){
     this.setText(name);
     this.memory = memory;
     this.addActionListener(this);
   }
   
+  /**
+  * Kickas igång när någon klickat på en knapp
+  * Kallar i sin tur på memorys save-metod
+  * @param e, Knappen som klickas på
+  */
   public void actionPerformed(ActionEvent e){
     this.memory.save();
   }
